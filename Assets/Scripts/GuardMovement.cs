2bf51ef9671201388a5e73a09373b7650aa4e0f5
DcentGaming <dabarker2000@yahoo.com>
2025-03-05 13:13:55 -0500

    added
    Guard movement. need to update route

--- Assets/Scripts/GuardMovement.cs
+++ Assets/Scripts/GuardMovement.cs
@@ -1,21 +1,22 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardMovement : MonoBehaviour
 {
-    public CharacterController Guard;
-    Vector3 moveDir;
-    public float speed = 5.0f;
-
+    public Transform Post_1;
+    public Transform Post_2;
+    private NavMeshAgent agent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = Post_1.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveDir = new Vector3(InputManager.Instance.Move.x, 0, InputManager.Instance.Move.y) * Time.deltaTime * speed;
-        Guard.Move(moveDir);
+        if (agent.destination == Post_1.position) agent.destination = Post_2.position;
+        else agent.destination = Post_1.position;
     }
 }
